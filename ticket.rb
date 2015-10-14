85f1859319cc4c73310b84cfd06e339a4bcb208a
itgsod-mille-elfver <mille.elfver@elev.it-gymnasiet.se>
2015-10-14 11:07:31 +0200

    Create ticket.rb

---
+++ ticket.rb
@@ -0,0 +1,8 @@
+def ask_age
+puts "Enter you age:"
+age = gets.to_i
+return age
+end
+
+#gets.to_r = real som betyder decimal i programmering
+#gets.to_i = intergerer som betyder heltal i programmering
